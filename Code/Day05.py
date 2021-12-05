90f5b2c0db2edd50744617a2a9a5f322f142eb1a
Alexandre Hassan <alexhassan@live.ca>
2021-12-05 12:46:39 -0500

    Initial day 05 part 2 solution

--- Code/Day05.py
+++ Code/Day05.py
@@ -9,10 +9,26 @@ import re
 
 class Vent:
     def __init__(self, line: str):
-        self.x1, self.x2 = self.order(int(line[0]), int(line[2]))
-        self.y1, self.y2 = self.order(int(line[1]), int(line[3]))
-        self.isVertical = self.y1 == self.y2
-        self.isHorizontal = self.x1 == self.x2
+        self.x1, self.y1, self.x2, self.y2 = int(
+            line[0]), int(line[1]), int(line[2]), int(line[3])
+        self.isVertical = self.x1 == self.x2
+        self.isHorizontal = self.y1 == self.y2
+        if self.isHorizontal or self.isVertical:
+            self.x1, self.x2 = self.order(self.x1, self.x2)
+            self.y1, self.y2 = self.order(self.y1, self.y2)
+        self.isDiagonal = not self.isVertical and not self.isHorizontal
+        self.posDiagonal = self.positiveSlope()
+        if self.isDiagonal:
+            if self.posDiagonal:
+                self.x1, self.x2 = self.order(self.x1, self.x2)
+                self.y1, self.y2 = self.order(self.y1, self.y2)
+            else:
+                self.x1, self.x2 = self.order(self.x1, self.x2)
+                self.y2, self.y1 = self.order(self.y1, self.y2)
+
+    def positiveSlope(self) -> bool:
+        return self.x2 - self.x1 > 0 and self.y2 - self.y1 > 0 or \
+            self.x2 - self.x1 < 0 and self.y2 - self.y1 < 0
 
     def part1Count(self):
         return self.isHorizontal or self.isVertical
@@ -53,14 +69,49 @@ def part1() -> int:
 
 
 def part2() -> int:
-    pass
+    lines = [re.split(',| ->', i) for i in input]
+    vents = [Vent(i) for i in lines]
+    grid = [[0 for i in range(1000)] for j in range(1000)]
+
+    for vent in vents:
+        # print("-"*20)
+        # print(vent)
+        # for row in grid:
+        #     print(row)
+        if vent.isVertical:
+            for i in range(vent.y1, vent.y2 + 1):
+                grid[i][vent.x1] += 1
+        elif vent.isHorizontal:
+            for i in range(vent.x1, vent.x2+1):
+                grid[vent.y1][i] += 1
+        else:
+            if vent.posDiagonal:
+                row, col = vent.y1, vent.x1
+                for i in range(vent.x2 - vent.x1 + 1):
+                    grid[row][col] += 1
+                    row += 1
+                    col += 1
+            else:
+                row, col = vent.y1, vent.x1
+                for i in range(vent.x2 - vent.x1 + 1):
+                    grid[row][col] += 1
+                    row -= 1
+                    col += 1
+    count = 0
+
+    for row in grid:
+        # print(row)
+        count += sum(col >= 2 for col in row)
+
+    return count
 
 
 def main():
-    # Part 1: 3633500
-    print(f"Part 1: {part1()}")
+    # Part 1: 7468
+    # print(f"Part 1: {part1()}")
     # Part 2: 4550283
-    # print(f"Part 2: {part2()}")
+    print(f"Part 2: {part2()}")
+    # Too high: 23482
 
     # # Part 1: 0.0010945069999999998s
     # print(f"Part 1: {time_function(part1)}s")
