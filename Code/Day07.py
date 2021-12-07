becc9b8ef2a1beca594c575124c2d95f007d219e
Alexandre Hassan <alexhassan@live.ca>
2021-12-07 01:12:36 -0500

    day 07 part 1 working

---
+++ Code/Day07.py
@@ -0,0 +1,85 @@
+"""
+For problem statement:
+    https://adventofcode.com/2021/day/3
+@author: Alexandre Hassan
+"""
+from Common import get_lines, time_function
+
+
+def sequence(num: int) -> int:
+    return num * (num + 1) // 2
+
+
+def calc(num: int, toCheck: list) -> int:
+    return sum(list(map(lambda x: abs(x - num), toCheck)))
+
+
+def calc_2(num: int, toCheck: list) -> int:
+    return sum(list(map(lambda x: sequence(abs(x - num)), toCheck)))
+
+
+def part1() -> int:
+    pos = list(map(int, (lines[0].split(","))))
+    pos.sort()
+    top = len(pos) - 1
+    bottom = 0
+    middle = (top + bottom) // 2
+
+    while True:
+        top_dist = calc(pos[top], pos)
+        bottom_dist = calc(pos[bottom], pos)
+
+        if top_dist < bottom_dist:
+            bottom = middle
+        else:
+            top = middle
+        if top_dist == bottom_dist:
+            return top_dist
+
+
+def part2() -> int:
+    pos = list(map(int, (lines[0].split(","))))
+    pos.sort()
+    top = len(pos) - 1
+    bottom = 0
+    middle = (top + bottom) // 2
+
+    while True:
+        top_dist = calc_2(pos[top], pos)
+        print(f"top_dist {top}: {top_dist}")
+        bottom_dist = calc_2(pos[bottom], pos)
+        print(f"top_dist {top}: {bottom_dist}")
+
+        if top_dist < bottom_dist:
+            bottom = middle
+        else:
+            top = middle
+        if top_dist == bottom_dist:
+            return top_dist
+
+
+def test():
+    part1_result = part1()
+    print(f"Part 1: {part1_result}")
+    # assert part1_result == 364898
+
+    part2_result = part2()
+    print(f"Part 2: {part2_result}")
+
+
+def main():
+    # Part 1: 351092
+    print(f"Part 1: {part1()}")
+    # Part 2: 1595330616005
+    print(f"Part 2: {part2()}")
+
+    # # Part 1: 0.00012867999999999997s
+    # print(f"Part 1: {time_function(part1)}s")
+    # # Part 2: 0.00030791799999999996s
+    # print(f"Part 2: {time_function(part2)}s")
+
+
+lines = get_lines("Inputs/Day07_sample.txt")
+
+if __name__ == "__main__":
+    test()
