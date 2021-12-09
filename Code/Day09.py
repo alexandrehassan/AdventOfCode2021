8d3c86b80bf3b094bcb4e8b539113943828c0fa1
Alexandre Hassan <alexhassan@live.ca>
2021-12-09 16:11:49 -0500

    Fix part 1

--- Code/Day09.py
+++ Code/Day09.py
@@ -19,24 +19,25 @@ def product_largest_3(nums: list) -> list:
     return nums[0] * nums[1] * nums[2]
 
 
-def get_adjecent_points(row: int, col: int) -> list:
-    adjecent_points = []
+def get_adjacent_points(row: int, col: int, num_lines: int, num_col: int) -> list:
+    adjacent_points = []
     if row > 0:
-        adjecent_points.append((row - 1, col))
+        adjacent_points.append((row - 1, col))
     if row < num_lines - 1:
-        adjecent_points.append((row + 1, col))
+        adjacent_points.append((row + 1, col))
     if col > 0:
-        adjecent_points.append((row, col - 1))
+        adjacent_points.append((row, col - 1))
     if col < num_col - 1:
-        adjecent_points.append((row, col + 1))
-    return adjecent_points
+        adjacent_points.append((row, col + 1))
+    return adjacent_points
 
 
 def get_neighbors(point: tuple, grid: list) -> list:
     neighbors = set()
     value = grid[point[0]][point[1]]
-    adjecent_points = get_adjecent_points(point[0], point[1])
-    for adj_point in adjecent_points:
+    adjacent_points = get_adjacent_points(
+        point[0], point[1], len(grid), len(grid[0]))
+    for adj_point in adjacent_points:
         if grid[adj_point[0]][adj_point[1]] > value and \
                 grid[adj_point[0]][adj_point[1]] != 9:
             neighbors.add(adj_point)
@@ -44,9 +45,9 @@ def get_neighbors(point: tuple, grid: list) -> list:
 
 
 def is_low_point(row: int, col: int, grid: list) -> bool:
-    adjecent_points = get_adjecent_points(row, col)
-    for point in adjecent_points:
-        if grid[point[0]][point[1]] < grid[row][col]:
+    adjacent_points = get_adjacent_points(row, col, len(grid), len(grid[0]))
+    for point in adjacent_points:
+        if grid[point[0]][point[1]] <= grid[row][col]:
             return False
     return True
 
@@ -101,18 +102,17 @@ def part2(lines: list) -> int:
 def main():
     lines = get_lines("Inputs/Day09.txt")
     global num_col
-    num_col = len(lines[0])
     global num_lines
-    num_lines = len(lines)
+
     # Part 1:
     print(f"Part 1: {part1(lines)}")
     # Part 2:
     print(f"Part 2: {part2(lines)}")
 
-    # # Part 1:
-    # print(f"Part 1: {time_function(lambda: part1(lines))}s")
-    # # Part 2: 0.03372787s
-    # print(f"Part 2: {time_function(lambda: part2(lines))}s")
+    # Part 1: 0.02289861800149083s
+    print(f"Part 1: {time_function(lambda: part1(lines))}s")
+    # Part 2: 0.056208234000951054s
+    print(f"Part 2: {time_function(lambda: part2(lines))}s")
 
 
 if __name__ == "__main__":
