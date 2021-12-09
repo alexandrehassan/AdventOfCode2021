d06916aeea46a1850f409803fcdf72cae49ef723
Alexandre Hassan <alexhassan@live.ca>
2021-12-09 15:45:23 -0500

    not working

--- Code/Day09.py
+++ Code/Day09.py
@@ -6,23 +6,12 @@ For problem statement:
 from Common import get_lines, time_function
 
 
-def get_neighbors(point: tuple, grid: list) -> list:
-    neighbors = set()
-    row, col = point
-    if row > 0 and grid[row - 1][col] > grid[row][col] \
-            and grid[row - 1][col] != 9:
-        neighbors.add((row - 1, col))
-    if col > 0 and grid[row][col - 1] > grid[row][col] \
-            and grid[row][col - 1] != 9:
-        neighbors.add((row, col - 1))
-    if row < len(grid) - 1 and grid[row + 1][col] > grid[row][col] \
-            and grid[row + 1][col] != 9:
-
-        neighbors.add((row + 1, col))
-    if col < len(grid[0]) - 1 and grid[row][col + 1] > grid[row][col] \
-            and grid[row][col + 1] != 9:
-        neighbors.add((row, col + 1))
-    return neighbors
+def valid(row: int, col: int, grid: list) -> bool:
+    try:
+        grid[row][col]
+        return True
+    except IndexError:
+        return False
 
 
 def product_largest_3(nums: list) -> list:
@@ -30,37 +19,44 @@ def product_largest_3(nums: list) -> list:
     return nums[0] * nums[1] * nums[2]
 
 
+def get_adjecent_points(row: int, col: int) -> list:
+    adjecent_points = []
+    if row > 0:
+        adjecent_points.append((row - 1, col))
+    if row < num_lines - 1:
+        adjecent_points.append((row + 1, col))
+    if col > 0:
+        adjecent_points.append((row, col - 1))
+    if col < num_col - 1:
+        adjecent_points.append((row, col + 1))
+    return adjecent_points
+
+
+def get_neighbors(point: tuple, grid: list) -> list:
+    neighbors = set()
+    value = grid[point[0]][point[1]]
+    adjecent_points = get_adjecent_points(point[0], point[1])
+    for adj_point in adjecent_points:
+        if grid[adj_point[0]][adj_point[1]] > value and \
+                grid[adj_point[0]][adj_point[1]] != 9:
+            neighbors.add(adj_point)
+    return neighbors
+
+
+def is_low_point(row: int, col: int, grid: list) -> bool:
+    adjecent_points = get_adjecent_points(row, col)
+    for point in adjecent_points:
+        if grid[point[0]][point[1]] < grid[row][col]:
+            return False
+    return True
+
+
 def find_lowpoints(grid: list) -> list:
     low_points = {}
     for row, line in enumerate(grid):
         for col, value in enumerate(line):
-            if col == len(line) - 1 and row == len(grid) - 1:
-                if grid[row - 1][col] > value and grid[row][col - 1] > value:
-                    low_points[(row, col)] = get_neighbors((row, col), grid)
-            elif col == len(line) - 1:
-                if grid[row - 1][col] > value and grid[row + 1][col] > value \
-                        and grid[row][col - 1] > value:
-                    low_points[(row, col)] = get_neighbors((row, col), grid)
-            elif row == len(grid) - 1:
-                if grid[row - 1][col] > value and grid[row][col + 1] > value \
-                        and grid[row][col - 1] > value:
-                    low_points[(row, col)] = get_neighbors((row, col), grid)
-            elif row == 0 and col == 0:
-                if grid[row + 1][col] > value and grid[row][col + 1] > value:
-                    low_points[(row, col)] = get_neighbors((row, col), grid)
-            elif row == 0:
-                if grid[row + 1][col] > value and grid[row][col + 1] > value \
-                        and grid[row][col - 1] > value:
-                    low_points[(row, col)] = get_neighbors((row, col), grid)
-            elif col == 0:
-                if grid[row + 1][col] > value and grid[row - 1][col] > value \
-                        and grid[row][col + 1] > value:
-                    low_points[(row, col)] = get_neighbors((row, col), grid)
-            else:
-                if grid[row + 1][col] > value and grid[row - 1][col] > value \
-                    and grid[row][col + 1] > value \
-                        and grid[row][col - 1] > value:
-                    low_points[(row, col)] = get_neighbors((row, col), grid)
+            if is_low_point(row, col, grid):
+                low_points[(row, col)] = get_neighbors((row, col), grid)
     return low_points
 
 
@@ -95,6 +91,8 @@ def part2(lines: list) -> int:
                 low_points[low_point].update(neighbors)
                 to_check.update(neighbors)
                 checked.add(current)
+            to_check = set(filter(lambda x: x not in checked, to_check))
+
         basin_sizes.append(len(low_points[low_point]) + 1)
 
     return product_largest_3(basin_sizes)
@@ -102,15 +100,19 @@ def part2(lines: list) -> int:
 
 def main():
     lines = get_lines("Inputs/Day09.txt")
+    global num_col
+    num_col = len(lines[0])
+    global num_lines
+    num_lines = len(lines)
     # Part 1:
     print(f"Part 1: {part1(lines)}")
     # Part 2:
     print(f"Part 2: {part2(lines)}")
 
-    # Part 1:
-    print(f"Part 1: {time_function(lambda: part1(lines))}s")
-    # Part 2:
-    print(f"Part 2: {time_function(lambda: part2(lines))}s")
+    # # Part 1:
+    # print(f"Part 1: {time_function(lambda: part1(lines))}s")
+    # # Part 2: 0.03372787s
+    # print(f"Part 2: {time_function(lambda: part2(lines))}s")
 
 
 if __name__ == "__main__":
