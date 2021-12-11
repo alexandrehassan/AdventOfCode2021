52c11827c08582065019cba7072d5a0bc36fc584
Alexandre Hassan <alexhassan@live.ca>
2021-12-11 16:07:16 -0500

    small changes

--- Code/Day11.py
+++ Code/Day11.py
@@ -10,59 +10,65 @@ from Common import get_lines, time_function, traverse_grid, \
 GRID_SIZE = (10, 10)
 
 
-def take_step(energy_levels: list) -> int:
-    flashes = []
-    for coord in traverse_grid(*GRID_SIZE):
-        row, col = coord
-        energy_levels[row][col] += 1
-        if energy_levels[row][col] == 10:
-            flashes.append(coord)
+def take_step(grid: list) -> int:
+    do_flashes(grid, will_flash(grid))
+    return count_flashes(grid)
 
-    while len(flashes) > 0:
-        flash_position = flashes.pop(0)
-        for coord in adjacent_coordinates(*flash_position, GRID_SIZE, True):
-            row, col = coord
-            energy_levels[row][col] += 1
-            if energy_levels[row][col] == 10:
-                flashes.append(coord)
 
+def count_flashes(grid):
     number_of_flashes = 0
     for coord in traverse_grid(*GRID_SIZE):
         row, col = coord
-        if energy_levels[row][col] >= 10:
+        if grid[row][col] >= 10:
             number_of_flashes += 1
-            energy_levels[row][col] = 0
-
+            grid[row][col] = 0
     return number_of_flashes
 
 
-def do_steps(grid: list, required_steps: int, part1: bool) -> int:
-    steps = 0
+def do_flashes(grid, to_flash):
+    while len(to_flash) > 0:
+        flash_position = to_flash.pop(0)
+        for coord in adjacent_coordinates(*flash_position, GRID_SIZE, True):
+            row, col = coord
+            grid[row][col] += 1
+            if grid[row][col] == 10:
+                to_flash.append(coord)
 
-    part1_res = 0
+
+def will_flash(grid):
+    flashes = []
+    for coord in traverse_grid(*GRID_SIZE):
+        row, col = coord
+        grid[row][col] += 1
+        if grid[row][col] == 10:
+            flashes.append(coord)
+    return flashes
+
+
+def do_steps(grid: list, steps_total: int, part1: bool) -> int:
+    steps_done, part1_res = 0, 0
+
     part2_res = None
 
     while part2_res is None:
         number_of_flashes = take_step(grid)
-        steps += 1
+        steps_done += 1
 
-        if steps <= required_steps:
+        if steps_done <= steps_total:
             part1_res += number_of_flashes
 
         if number_of_flashes == GRID_SIZE[0]**2:
-            part2_res = steps
+            part2_res = steps_done
 
     return part1_res if part1 else part2_res
 
 
 def part1(lines: list) -> int:
-    grid = get_int_grid(lines)
-    return do_steps(grid, 100, part1=True)
+    return do_steps(get_int_grid(lines), 100, part1=True)
 
 
 def part2(lines: list) -> int:
-    grid = get_int_grid(lines)
-    return do_steps(grid, 100, part1=False)
+    return do_steps(get_int_grid(lines), 100, part1=False)
 
 
 def main():
@@ -72,9 +78,9 @@ def main():
     # # Part 2: 279
     print(f"Part 2: {part2(lines)}")
 
-    # Part 1: 0.049193955999944
+    # Part 1: 0.024099096s
     print(f"Part 1: {time_function(lambda: part1(lines))}s")
-    # Part 2: 0.050320960999961244s
+    # Part 2: 0.023895938999999995s
     print(f"Part 2: {time_function(lambda: part2(lines))}s")
 
 
